b078619825411f7c03fc046ba93dcdfbcea36f99
Harm Aarts <harm@mindshards.com>
2013-11-14 15:20:30 +0100

    Why not use the global variable?

--- grails-app/services/com/recomdata/transmart/data/export/GeneExpressionDataService.groovy
+++ grails-app/services/com/recomdata/transmart/data/export/GeneExpressionDataService.groovy
@@ -451,7 +451,7 @@ class GeneExpressionDataService {
         log.info("started file writing")
         def output;
 
-        FileWriterUtil writerUtil = new FileWriterUtil(studyDir, fileName, jobName, "mRNA", "Processed_Data", '\t');
+        FileWriterUtil writerUtil = new FileWriterUtil(studyDir, fileName, jobName, "mRNA", "Processed_Data", valueDelimiter);
         File outFile = writerUtil.outputFile
         output = outFile.newWriter(true)
         output << constructHeaderRow(splitAttributeColumn, includePathwayInfo)
